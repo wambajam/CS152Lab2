0866e3c31cdb38bd59a264f9e6214e8237e0510c
Randy Berlin <173215448+randyberlin@users.noreply.github.com>
2024-07-11 16:39:17 -0700

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,3 +8,10 @@ class UsersController < ApplicationController
         format.csv { send_data @users.to_csv, filename: "users-#{Date.today}.csv" }
       end
     end
+
+def create
+  user= User.find_by (username:params[:username])
+  if user && user.authenticate (params[:password])
+    session [:user_id] = user.indexredirect_to user_path_(user)
+  end
+end
